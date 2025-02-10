beb7d9f902ffc3871e377c621f80bc285430312c
anas-mohamed342020 <ans192174@fci.bu.edu.eg>
2025-02-10 11:08:52 +0200

    fix env

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import { bootstrap } from './src/app.controller.js'
+import { config } from "dotenv";
+config()
 const app = express()
 const port = process.env.PORT
 bootstrap(app, express)
--- src/utils/multer/cloudinary.js
+++ src/utils/multer/cloudinary.js
@@ -1,5 +1,6 @@
 import { v2 as cloudinary } from 'cloudinary';
-
+import { config } from "dotenv";
+config()
 console.log({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.API_KEY,
